ee730667678ef2038b1f57fb51fb96d3ff19a71d
vctoria sant <programadorasantvic@gmail.com>
2025-05-28 13:14:56 +0000

    teste de operadores de atribuiçao

---
+++ operadoresatribuicao.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+//operador de atribuição
+
+int main(){
+
+/*
+Atribuição simples (=)
+Atribuição com soma (+=)
+Atribuição com subtração (-=)
+Atribuição com multiplicação (*=)
+Atribuição com divisão(/=)
+*/
+
+int numero1 = 10, numero2, resultado;
+
+//resultado = 
+resultado = 10;
+printf("resultado: %d\n", resultado);
+//resultado = resultado + 20;
+resultado += 20;
+printf("resultado: %d\n", resultado);
+//resultado = resultado - numero1;
+resultado -= numero1;
+printf("resultado: %d\n", resultado);
+//resultado = resultado * 5;
+resultado *= 5;
+printf("resultado: %d\n", resultado);
+//resultado = resultado / 2;
+resultado /= 2;
+printf("resultado: %d\n", resultado);
+
+
+
+
+
+
+return 0;
+}
